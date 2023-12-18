9db057a8b3ea9b7d66f0ae573fbe3f309b222d30
YerrajeniReddy123 <mpldilip1099@gmail.com>
2023-12-18 16:42:56 +0530

    ydr

---
+++ EqualsVsOper/Demo.java
@@ -0,0 +1,8 @@
+class Demo{
+    public static void main(String[] args){
+        String s1=new String("hello");
+        s1=s1.concat("World");
+        System.out.println(s1);
+    }
+
+}
---
+++ EqualsVsOper/Test.java
@@ -0,0 +1,9 @@
+class Test{
+    public static void main(String[] args){
+        String s1=new String("Rahul");
+        s1.concat("Gandhi");
+
+        System.out.println(s1);
+        System.out.println(s1.toString());
+    }   
+}
