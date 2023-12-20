414e7aef22deeee17db2a7dafaf605555809dcf3
YerrajeniReddy123 <mpldilip1099@gmail.com>
2023-12-20 09:14:49 +0530

    ydr

---
+++ AccessModifier/Final.java
@@ -0,0 +1,20 @@
+// final class Outer{
+//         final int x=10;
+//         final void m1(){
+        
+//         }
+//     public static void main (String[] args){
+       
+//     }
+
+// }
+
+final class Test{
+    
+    public static void main (String[] args){
+        final int x;
+        x=200;
+       // x=300;
+        System.out.println(x);
+    }
+}
