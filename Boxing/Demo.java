dfcbeee7155d7da490bfe67c006af432dbf63db1
YerrajeniReddy123 <mpldilip1099@gmail.com>
2023-12-22 21:49:09 +0530

    ydr

--- Boxing/Demo.java
+++ Boxing/Demo.java
@@ -10,19 +10,43 @@ class Demo{
         char ch2=ch1.charValue(); //UnBoxing
         char ch3=ch1; //UnBoxing
 
-        String s1="YDR";
+        //String to Primitive
+        String s1="123";
         int d=Integer.parseInt(s1);
+        //int f=s1; (Not possible)
 
+        //Not possible
+        String s2="Y";
+        //char e=Character.parseChar(s2);
+
+        //Primitive to String
+        int g=100;
+        String s3=Integer.toString(g);
+
+        //Object to String
+        Integer h=10;
+        String s4=Integer.toString(h);
+
+        //String to Object
+        String s5="20";
+        Integer i=Integer.parseInt(s5);
 
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
         
-
         System.out.println(ch1);
         System.out.println(ch2);
         System.out.println(ch3);
 
         System.out.println(d);
+
+        System.out.println(s3);
+
+        System.out.println(h);
+        System.out.println(s4);
+
+        System.out.println(s5);
+        System.out.println(i);
     }
 }
