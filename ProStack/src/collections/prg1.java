a4888400749a7a5ec128df72053a6eae42dc7734
YerrajeniReddy123 <mpldilip1099@gmail.com>
2024-01-08 21:46:12 +0530

    ydr

---
+++ ProStack/src/collections/prg1.java
@@ -0,0 +1,13 @@
+package collections;
+import java.util.ArrayList;
+public class prg1 {
+
+	public static void main(String[] args) {
+		ArrayList enames=new ArrayList();
+		enames.add("Rahul");
+//		System.out.println(enames);
+		
+
+	}
+
+}
