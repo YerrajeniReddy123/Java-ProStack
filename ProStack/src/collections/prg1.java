08b1ca577028e46d119c735b6af412a9132953b2
YerrajeniReddy123 <mpldilip1099@gmail.com>
2024-01-09 21:30:36 +0530

    ydr

--- ProStack/src/collections/prg1.java
+++ ProStack/src/collections/prg1.java
@@ -4,10 +4,24 @@ public class prg1 {
 
 	public static void main(String[] args) {
 		ArrayList enames=new ArrayList();
-		enames.add("Rahul");
-//		System.out.println(enames);
+		enames.add("Rahul");		
+		enames.add(101);
+		enames.add(101);
+		enames.add(45.564);
+		enames.add(true);
+		enames.add(null);
 		
-
+		System.out.println(enames);
+		System.out.println(enames.size());
+		
+		for(Object ename:enames) {
+			System.out.println(ename);
+		}
+		
+		System.out.println(enames.get(3)); //to read the element value
+	//	System.out.println(enames.set(0, "Rahul Gandhi"));
+		enames.set(0, "Rahul Gandhi");
+		System.out.println(enames);
 	}
 
 }
